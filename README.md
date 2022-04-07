805f651e533b567ce384202f447e3133095cefa6
GaoJieo <lgj235@gmail.com>
2022-04-07 22:16:00 +0800

    更新yolov5-5.0版

---
+++ README.md
@@ -0,0 +1,9 @@
+# yolov5-tensorrtx-VideoCapture
+The tensorrtx source code modification for Yolov5 is used for video stream reasoning  
+
+https://github.com/ultralytics/yolov5
+https://github.com/wang-xinyu/tensorrtx
+
+根据上方的源码做了简单的修改所得，用于视频流推理
+使用方法参考：https://blog.csdn.net/weixin_46596757/article/details/123904712?spm=1001.2014.3001.5502
+
---
+++ utils.h
@@ -0,0 +1,35 @@
+#ifndef TRTX_YOLOV5_UTILS_H_
+#define TRTX_YOLOV5_UTILS_H_
+
+//  #include <dirent.h>
+#include <opencv2/opencv.hpp>
+
+static inline cv::Mat preprocess_img(cv::Mat& img, int input_w, int input_h) {
+    int w, h, x, y;
+    float r_w = input_w / (img.cols*1.0);
+    float r_h = input_h / (img.rows*1.0);
+    if (r_h > r_w) {
+        w = input_w;
+        h = r_w * img.rows;
+        x = 0;
+        y = (input_h - h) / 2;
+    } else {
+        w = r_h * img.cols;
+        h = input_h;
+        x = (input_w - w) / 2;
+        y = 0;
+    }
+    cv::Mat re(h, w, CV_8UC3);
+    cv::resize(img, re, re.size(), 0, 0, cv::INTER_LINEAR);
+    cv::Mat out(input_h, input_w, CV_8UC3, cv::Scalar(128, 128, 128));
+    re.copyTo(out(cv::Rect(x, y, re.cols, re.rows)));
+    return out;
+}
+
+static inline int read_files_in_dir(const char *p_dir_name, std::vector<std::string> &file_names) {
+    
+    return 0;
+}
+
+#endif  // TRTX_YOLOV5_UTILS_H_
+
